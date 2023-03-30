383259b072ad4ad2c80aae2623dbae1c071867af
zabih <107562768+minhaj-313@users.noreply.github.com>
2023-03-31 02:41:53 +0530

    Create 2nd.py

---
+++ 2nd.py
@@ -0,0 +1,14 @@
+# Variable
+a = 10
+b = 10 * a
+print(b)
+
+# User INPUT Integer
+i =(input("enter number="))
+print(i)
+
+
+# String Variable
+# str1 = "Shaikh"
+# str2 = "Minhaj"
+# print(int (str1 + str2))
