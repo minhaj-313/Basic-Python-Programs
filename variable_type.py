6a96efeb34311d682f2cb3b63a1293b91873ba03
Tyler Le <tylerle99@yahoo.com>
2023-04-02 09:54:59 -0700

    print variable types

---
+++ variable_type.py
@@ -0,0 +1,17 @@
+# Define variables of different types
+my_int = 10
+my_float = 3.14
+my_string = 'hello world'
+my_bool = True
+my_list = [1, 2, 3, 4]
+my_tuple = (5, 6, 7, 8)
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+
+# Print the types of each variable
+print(type(my_int))  # Output: <class 'int'>
+print(type(my_float))  # Output: <class 'float'>
+print(type(my_string))  # Output: <class 'str'>
+print(type(my_bool))  # Output: <class 'bool'>
+print(type(my_list))  # Output: <class 'list'>
+print(type(my_tuple))  # Output: <class 'tuple'>
+print(type(my_dict))  # Output: <class 'dict'>
