5d4ad1da8b426d57a54d768b6f627bb605ac1024
Tyler Le <tylerle99@yahoo.com>
2023-04-02 18:54:33 -0700

    merge sort implementation

---
+++ merge_sort.py
@@ -0,0 +1,34 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
+    
+    left = merge_sort(left)
+    right = merge_sort(right)
+    
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i = 0
+    j = 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+            
+    result += left[i:]
+    result += right[j:]
+    return result
+
+# Example Usage
+arr = [3, 6, 1, 9, 2, 8, 5, 7, 4]
+sorted_arr = merge_sort(arr)
+print("Original Array: ", arr)
+print("Sorted Array: ", sorted_arr)
